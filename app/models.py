2a0fe2714be9d87489c4730bc8a46a126db270f7
dianaboiangiu <boiangiudiana58@gmail.com>
2020-12-02 08:23:13 +0200

    Add Comment and Country models

--- app/models.py
+++ app/models.py
@@ -4,6 +4,16 @@ from django.db import models
 
 User = get_user_model()
 
+class Country(models.Model):
+    code = models.CharField(max_length=3, primary_key=True)
+    name = models.CharField(max_length=70)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Countries"
+
 class Post(models.Model):
     text = models.CharField(max_length=200)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE,related_name='posts')
@@ -13,8 +23,19 @@ class Post(models.Model):
     def __str__(self):
         return "{} created by {} at {}".format(self.text, self.created_by.username, self.created_at)
 
+class Comment(models.Model):
+    text = models.CharField(max_length=200)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "{} created by {} at {}".format(self.text, self.created_by.username, self.created_at)
+
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     birthday = models.DateField(blank=True, null=True)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE, related_name="profiles", blank=True, null=True)
     friend_requests = models.ManyToManyField(User, related_name='friend_requests', blank=True)
     friends = models.ManyToManyField(User, related_name='friends', blank=True)
